98eedb73fd79fda4effc925f89a6ae5704fb3bec
DhrumilP2905 <dhrumilpanchal295@gmail.com>
2025-02-25 03:01:43 +0530

    update backend

--- server.js
+++ server.js
@@ -37,7 +37,10 @@ const Message =
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://chat-frontend-dun-seven.vercel.app/",
+    ],
     methods: ["GET", "POST"],
     credentials: true,
   },
